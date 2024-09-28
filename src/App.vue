7752199a1aad8c0b4531b5bb9725e79a6b838fe3
JoãoPaulo-dev-lab <paulo.j7.jp89@gmail.com>
2024-09-28 01:27:07 -0300

    change App Component

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ import Television from 'vue-material-design-icons/Television.vue';
 import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
 import Plus from 'vue-material-design-icons/Plus.vue';
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import BellOutline from 'vue-material-design-icons/BellOutline.vue'
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue'
 
 import { useMovieStore } from './stores/movie';
 import { storeToRefs } from 'pinia';
@@ -38,47 +40,72 @@ onMounted(() => {
   <div class="w-full h-screen bg-black">
     <header class="flex items-center w-full py-2 px-14 justify-between text-white">
       <div class="flex items-center gap-x-12">
-        <div >
+        <div>
           <img class="max-w-24" src="../public/Netflix-Logo.png" alt="">
         </div>
         <div class="flex gap-x-4">
           <li class="list-none">
-           <a href="" class="no-underline font-semibold text-slate-300 text-[15px]">Início</a>
+            <a href="" class="no-underline hover:text-white font-semibold text-slate-300 text-[15px]">Início</a>
           </li>
           <li class="list-none">
-           <a href="" class="no-underline font-semibold text-slate-300 text-[15px]">Séries</a>
+            <a href="" class="no-underline hover:text-white font-semibold text-slate-300 text-[15px]">Séries</a>
           </li>
           <li class="list-none">
-           <a href="" class="no-underline font-semibold text-slate-300 text-[15px]">Filmes</a>
+            <a href="" class="no-underline hover:text-white font-semibold text-slate-300 text-[15px]">Filmes</a>
           </li>
           <li class="list-none">
-           <a href="" class="no-underline font-semibold text-slate-300 text-[15px]">Bombando</a>
+            <a href="" class="no-underline hover:text-white font-semibold text-slate-300 text-[15px]">Bombando</a>
           </li>
           <li class="list-none">
-           <a href="" class="no-underline font-semibold text-slate-300 text-[15px]">Minha lista</a>
+            <a href="" class="no-underline hover:text-white font-semibold text-slate-300 text-[15px]">Minha lista</a>
           </li>
           <li class="list-none">
-           <a href="" class="no-underline font-semibold text-slate-300 text-[15px]">Navegar por idiomas</a>
+            <a href="" class="no-underline hover:text-white font-semibold text-slate-300 text-[15px]">Navegar por
+              idiomas</a>
           </li>
         </div>
       </div>
       <div class="flex items-center gap-x-5">
         <div>
-          <Magnify class="cursor-pointer"/>
+          <Magnify class="cursor-pointer  text-slate-300 hover:text-white" />
         </div>
         <div>
-          <a href="" class="font-semibold text-slate-300 text-[15px] no-underline">
+          <a href="" class="font-semibold text-slate-300 text-[15px] no-underline hover:text-white">
             Infantil
           </a>
         </div>
         <div>
-          C
+          <div class="cursor-pointer text-slate-300 hover:text-white">
+            <BellOutline />
+          </div>
         </div>
-        <div>
-          D
+        <div class="flex items-center cursor-pointer">
+          <img src="../public/user.png" alt="">
+          <div class="cursor-pointer text-slate-300 hover:text-white">
+            <ChevronDown />
+          </div>
         </div>
       </div>
     </header>
+    <div v-if="!showFullVideos" class="">
+      <div class="fixed flex z-20  right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
+        <div class="absolute z-30 h-[600px] left-[120px] w-[77%] right-0 " />
+
+        <MovieComponent v-if="movie" :movie="movie" />
+        <video v-if="movie" :src="`/videos/${movie.name}.mp4`" autoplay loop
+          class="absolute z-0 w-full h-[600px] right-0 top-0" />
+      </div>
+
+      <!-- <div class="absolute z-20 h-[70%] left-[120px] w-[100%] right-0 bottom-0" /> -->
+    </div>
+    <!-- <div v-if="showFullVideos">
+      <div @click="showFullVideos = false"
+        class="absolute z-50 p-2 m-4 bg-white bg-opacity-50 rounded-full cursor-pointer">
+        <ChevronLeft fillColor="#FFFFFF" :size="40" />
+      </div>
+      <video :src="`/videos/${movie?.name}.mp4`" autoplay loop controls
+        class="absolute z-0 w-[75%] h-[75%] object-fit" />
+    </div> 
     <!-- <div v-if="!showFullVideos" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
       <img class="absolute top-0 w-[35px] mt-10 ml-10 pb-4" src="/images/netflix-logo.png" alt="Netflix Logo">
       <div class="flex flex-col w-[150px] gap-y-[5vh] pt-14">
